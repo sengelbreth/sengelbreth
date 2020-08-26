b461fdb691c686447b476a7c78f1e81108c82e0f
sengelbreth <32447821+sengelbreth@users.noreply.github.com>
2020-08-26 12:37:05 +0200

    Add files via upload

---
+++ kea/Weather_app/precache-manifest.f762febe14951f96f50fcaeb06968930.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa545eda5ba504e2076fbfe8863259fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a5ee50271a2e94d1ff",
+    "url": "/static/css/2.cf12d109.chunk.css"
+  },
+  {
+    "revision": "c0bd0b48ca9d930d4795",
+    "url": "/static/css/main.70a9a41b.chunk.css"
+  },
+  {
+    "revision": "a5a5ee50271a2e94d1ff",
+    "url": "/static/js/2.71dec7b4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.71dec7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0bd0b48ca9d930d4795",
+    "url": "/static/js/main.3a39fed7.chunk.js"
+  },
+  {
+    "revision": "50d8c29dcf3f308d0158",
+    "url": "/static/js/runtime-main.46e75fce.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
